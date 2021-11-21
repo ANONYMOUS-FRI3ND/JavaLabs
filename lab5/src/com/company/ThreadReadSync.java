2381274b62ad95e82b6489f87c3a2f643db3256a
ANONYMOUS-FRI3ND <anonymous_fri3nd@mail.ru>
2021-11-21 20:27:00 +0400

    lab5
    lb

---
+++ lab5/src/com/company/ThreadReadSync.java
@@ -0,0 +1,21 @@
+package com.company;
+
+
+public class ThreadReadSync implements Runnable {
+    private GetSetSync sync;
+
+    public ThreadReadSync(GetSetSync s) {
+        sync = s;
+    }
+
+    @Override
+    public void run() {
+        try {
+            sync.read();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+// Чтение из массива (2 задание)
---
+++ lab5/src/com/company/ThreadWriteSync.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class ThreadWriteSync implements Runnable {
+    private GetSetSync sync;
+
+
+    public ThreadWriteSync(GetSetSync s) {
+        sync = s;
+    }
+
+
+
+    @Override
+    public void run() {
+        try {
+            sync.write();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+// Запись в массив (2 задание)
